c9d63a265302459fe81fa145dc1d050274d9e30d
Michael Hunt <mhunt429@gmail.com>
2023-04-27 19:27:13 -0400

    fix build?

--- utilities/DbMigrationRunner/ConnectionStrings.cs
+++ utilities/DbMigrationRunner/ConnectionStrings.cs
@@ -8,7 +8,7 @@ namespace DbMigrationRunner
 {
     public class ConnectionStrings
     {
-        public string SelahDbLocal => Environment.GetEnvironmentVariable("Selah_Db_Local_Connection_String");
+        public string SelahDbLocal => Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
         public string SelahDbTest => Environment.GetEnvironmentVariable("Selah_Db_Integration_Test_Connection_String");
     }
 }
