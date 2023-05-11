4d05332e71b3b99eceb685c9e3c9dd257da7a05c
Michael Hunt <mhunt429@gmail.com>
2023-05-10 21:14:20 -0400

    Add income statement tables and indexes

---
+++ utilities/DbMigrationRunner/migrations/V20230510205347__CREATE_INCOME_STATEMENT_TABLE.sql
@@ -0,0 +1,14 @@
+CREATE TABLE income_statement(
+    id BIGSERIAL PRIMARY KEY,
+    user_id uuid REFERENCES app_user(id) ON DELETE CASCADE,
+    statement_start_date TIMESTAMP,
+    statement_end_state TIMESTAMP,
+    total_pay DECIMAL
+);
+
+/*
+ROLLBACK 
+DROP INDEX income_statement_user
+DROP TABLE income_statement
+DELETE FROM changelog WHERE name = 'V20230510205347__CREATE_INCOME_STATEMENT_TABLE.sql'
+*/
